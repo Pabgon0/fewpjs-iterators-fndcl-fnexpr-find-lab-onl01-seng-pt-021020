649aab4936ce5001285c2ebbc554fd50aa9b29f1
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-02 21:14:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  let answer = array.find( array => array.result === "W")
+  return !!answer ? answer.year : undefined
 }
